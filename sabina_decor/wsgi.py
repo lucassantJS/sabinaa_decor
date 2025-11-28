d781918f22e6029302eb884243d7db59d58d2a6e
Lucas Henrique dos Santos <lucashenri0231@gmail.com>
2025-11-27 22:55:36 -0300

    arrumando email

--- sabina_decor/wsgi.py
+++ sabina_decor/wsgi.py
@@ -1,14 +1,15 @@
-"""
-WSGI config for sabina_decor project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
 import os
-
+import socket # <--- Adicionado
 from django.core.wsgi import get_wsgi_application
+
+# --- PATCH PARA FORÇAR IPV4 (CORREÇÃO DO ERRO 101) ---
+# O Gmail tenta usar IPv6, mas o servidor bloqueia. Isso obriga a usar IPv4.
+old_getaddrinfo = socket.getaddrinfo
+def new_getaddrinfo(*args, **kwargs):
+    responses = old_getaddrinfo(*args, **kwargs)
+    return [response for response in responses if response[0] == socket.AF_INET]
+socket.getaddrinfo = new_getaddrinfo
+# -----------------------------------------------------
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sabina_decor.settings')
 
