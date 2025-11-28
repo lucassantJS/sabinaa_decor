2ae0ceb809ba7ec4b8597b3f7927f9fa8a42ac62
Lucas Henrique dos Santos <lucashenri0231@gmail.com>
2025-11-27 22:27:19 -0300

    tentando email

--- sabina_decor/settings.py
+++ sabina_decor/settings.py
@@ -144,8 +144,9 @@ LOGOUT_REDIRECT_URL = '/inicio/'
 # --- EMAIL (CONFIGURAÇÃO GMAIL COM SENHA DE APP) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'lucashenri0231@gmail.com'
 EMAIL_HOST_PASSWORD = 'psprtrgkyzwguncj'
 DEFAULT_FROM_EMAIL = 'Sabina Decorações <lucashenri0231@gmail.com>'
